f8561f9a36576b77f85891c3c60dfdfd8248b93b
Afrozalam123 <alamafroz70400@gmail.com>
2025-06-22 16:49:09 +0530

    updated Index.js with new changes

--- Index.js
+++ Index.js
@@ -1,11 +1,17 @@
-import React from 'react'
+// Input: nums = [2,2,1]
+// Output: 1
 
-function Index() {
-  return (
-    <div>
-      console.log("Hello DSA!");
-    </div>
-  )
+function findSingleNumber(nums){
+  let result=0;
+  for(let i=0;i<nums.length;i++){
+    result^=nums[i]
+  }
+  return result;
 }
+let nums1=[2,2,1]
+let nums2=[4,1,2,1,2]
+let nums3=[1]
 
-export default Index
+console.log(findSingleNumber(nums1))
+console.log(findSingleNumber(nums2))
+console.log(findSingleNumber(nums3))
