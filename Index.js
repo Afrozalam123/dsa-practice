55a44bd49258adcf5ebc381033b6bb2a750b2624
Afrozalam123 <alamafroz70400@gmail.com>
2025-06-27 15:53:32 +0530

    Uncommented the code and fixed it

--- Index.js
+++ Index.js
@@ -1,20 +1,20 @@
 // Input: nums = [2,2,1]
 // Output: 1
 
-// function findSingleNumber(nums){
-//   let result=0;
-//   for(let i=0;i<nums.length;i++){
-//     result^=nums[i]
-//   }
-//   return result;
-// }
-// let nums1=[2,2,1]
-// let nums2=[4,1,2,1,2]
-// let nums3=[1]
+function findSingleNumber(nums){
+  let result=0;
+  for(let i=0;i<nums.length;i++){
+    result^=nums[i]
+  }
+  return result;
+}
+let nums1=[2,2,1]
+let nums2=[4,1,2,1,2]
+let nums3=[1]
 
-// console.log(findSingleNumber(nums1))
-// console.log(findSingleNumber(nums2))
-// console.log(findSingleNumber(nums3))
+console.log(findSingleNumber(nums1))
+console.log(findSingleNumber(nums2))
+console.log(findSingleNumber(nums3))
 
 
 // leetcode qn0=14 easy
