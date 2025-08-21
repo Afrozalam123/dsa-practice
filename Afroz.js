2a584700ba13bb3c8d45c45741692b491b941170
Afrozalam123 <alamafroz70400@gmail.com>
2025-08-21 12:04:44 +0530

    initial commit

--- Afroz.js
+++ Afroz.js
@@ -1,11 +1,82 @@
-let traffic="";   // please enter the color you will give the answer
+// let traffic="";   // please enter the color you will give the answer
 
-if(traffic==="red"){
-  console.log("you will stop the car")
-}if(traffic==="yellow"){
-  console.log("you will ready")
-}if(traffic==="green"){
-  console.log("you will go")
-}else{
-  console.log("you go Anywhere")
+// if(traffic==="red"){
+//   console.log("you will stop the car")
+// }if(traffic==="yellow"){
+//   console.log("you will ready")
+// }if(traffic==="green"){
+//   console.log("you will go")
+// }else{
+//   console.log("you go Anywhere")
+// }
+
+
+// function add(a,b){
+//   return a+b
+
+// }
+// console.log(add(12,5))
+
+
+// let arr=[
+//   {
+//     Name:"Afroz",
+//     Age:20,
+//     "Father-Name":"Md firoz"
+//   }
+// ];
+// console.log(Arr)
+
+
+// let arr = [
+//   {
+//     Name: "Afroz",
+//     Age: 20,
+//     "Father-Name": "Md Firoz"
+//   }
+// ];
+
+// console.log(arr[1].Age);
+
+
+// let str="madam"
+
+// function ispalindrome(str){
+//   let reversed=str.split("").reverse().join("")
+//   if(str===reversed){
+//     console.log(`${str} this is a palindrome`)
+//   }else{
+//     console.log(`${str} this is not palindrome number`)
+//   }
+//   return reversed
+// }
+// let str="madam"
+
+// console.log(ispalindrome(str))
+
+
+
+
+// function add(a,b,c,d){
+//   return a+b+c-d
+// }
+// console.log(add(12,4,5,3))
+
+
+
+
+function ispalindrome(str){
+  let reversed=str.split("").reverse().join("")
+  if(str===reversed){
+    console.log(` ${str} this is a palindrome`)
+  }else{
+    console.log(`${str} this is a not palindrome`)
+  }
+  return reversed;
 }
+let str="hello"
+let str1="madam"
+console.log(ispalindrome(str))
+console.log(ispalindrome(str1))
+
+
--- Index.js
+++ Index.js
@@ -238,14 +238,14 @@
 
 
 
-let traffic="";   // please enter the color you will give the answer
+// let traffic="";   // please enter the color you will give the answer
 
-if(traffic==="red"){
-  console.log("you will stop the car")
-}if(traffic==="yellow"){
-  console.log("you will ready")
-}if(traffic==="green"){
-  console.log("you will go")
-}else{
-  console.log("you go Anywhere")
-}
+// if(traffic==="red"){
+//   console.log("you will stop the car")
+// }if(traffic==="yellow"){
+//   console.log("you will ready")
+// }if(traffic==="green"){
+//   console.log("you will go")
+// }else{
+//   console.log("you go Anywhere")
+// }
